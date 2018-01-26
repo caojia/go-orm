5038a7a01751f417124a5ac666fde431e83b0366
luanxiaowang <luanxiaowang@guazi.com>
2018-01-26 16:29:52 +0800

    fix

--- utils.go
+++ utils.go
@@ -43,10 +43,10 @@ func changeSQLIn(sql string, args ...interface{}) (string, []interface{}) {
 }
 
 //检测select 的sql中的select函数时候存在limit，0表示就补上limit 2000，1表示补上limit 1
-func AddLimit(sql string,limitStatus int)(string,error){
+func addLimit(sql string,limitStatus int)string{
 	//判断select是否有limit这个关键字,检查子查询
-	if ok,_:=regexp.MatchString(`(?i)limit`,sql);ok{
-		return sql,nil
+	if ok,_ := regexp.MatchString(`(?i)limit`,sql);ok{
+		return sql
 	}
 	//最后一个批配项
 	switch limitStatus {
@@ -55,7 +55,7 @@ func AddLimit(sql string,limitStatus int)(string,error){
 	case 1:
 		sql +=" LIMIT 1 "
 	}
-	return sql,nil
+	return sql
 }
 
 //判断主键和自增列是否存在，存在就返回true，否则返回false
