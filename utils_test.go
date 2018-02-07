bd6e8d275a84efa55f3293b5fe6c05b7b41659ea
Lingchao Xin <douglarek@gmail.com>
2018-02-07 17:44:15 +0800

    Fix limit adding when encounter `` string

--- utils_test.go
+++ utils_test.go
@@ -1,11 +1,29 @@
 package orm
 
-import (
-	"testing"
-	"log"
-)
+import "testing"
 
-func TestAddLimit(t *testing.T) {
-	str:=addLimit(`select * from test_orm_a123 where test_id = ?   `,0)
-	log.Println(str)
+func Test_addLimit(t *testing.T) {
+	type args struct {
+		sql         string
+		limitStatus int
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{"t1", args{sql: `select * from test_orm_a123 where test_id = ? `, limitStatus: 0}, `select * from test_orm_a123 where test_id = ? LIMIT 2000 `},
+		{"t2", args{
+			sql: `
+			select * from test_orm_a123 where test_id = ?;
+			`,
+			limitStatus: 0}, `select * from test_orm_a123 where test_id = ? LIMIT 2000 `},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := addLimit(tt.args.sql, tt.args.limitStatus); got != tt.want {
+				t.Errorf("addLimit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
 }
